3c37c719c29806606e57e4efb377d88e7b788f50
Agustin Godinez <AgustinGodinez>
2023-12-28 20:47:02 -0600

    metadata
    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,17 @@ import "./globals.css";
 import { Footer, Navbar } from "@/components";
 
 export const metadata: Metadata = {
+  icons: {
+    icon: "/hero.png",
+  },
   title: "Car Nova",
   description: "Discover the best cars in the world.",
+  keywords: ["Discover", "Best cars", "Cars", "Search"],
+  openGraph: {
+    title: "Car Nova",
+    description: "Discover the best cars in the world.",
+    images: "/hero.png",
+  },
 };
 
 export default function RootLayout({
